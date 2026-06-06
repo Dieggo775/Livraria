1d837b38b1b846891941b4e4f607dc75eb935423
Dieggo775 <dieggo_taylor@hotmail.com>
2026-06-06 11:39:20 -0300

    Instalação da estrutura para criação da API

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+* Objetivo: API para realizar o CRUD de dados de Livros
+* Data: 06/06/2026
+* Versão: 1.0
+* Autor: Diego Araujo
+*/
+
+/* Roteiro para instalação e configuração das bibliotecas 
+* Express: npm install express --save (instalado para criar a API)
+* Cors: npm install cors --save (configuração de acesso da API)
+* Body-Parser: npm install body-parser --save (configuração para receber os dados do body no POST ou PUT)
+*
+* Fastfy: npm install fastify --save (OPCIONAL)
+*/
